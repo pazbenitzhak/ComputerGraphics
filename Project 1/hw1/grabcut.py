6597c55845f3bc538f400a73527cf2fc840ef9de
EldadLigishal <80391325+EldadLigishal@users.noreply.github.com>
2023-04-12 15:13:25 +0300

    Update GMMs function is done

--- Project 1/hw1/grabcut.py
+++ Project 1/hw1/grabcut.py
@@ -3,11 +3,14 @@ import cv2
 import argparse
 from sklearn.cluster import KMeans
 from sklearn.mixture import GMM
+import igraph as ig
 
 GC_BGD = 0 # Hard bg pixel
 GC_FGD = 1 # Hard fg pixel, will not be used
 GC_PR_BGD = 2 # Soft bg pixel
 GC_PR_FGD = 3 # Soft fg pixel
+CLUSTERS_NUM = 5 # Based on the instructions
+GAMMA = 50 # According to the article 
 
 
 # Define the GrabCut algorithm function
@@ -43,34 +46,100 @@ def grabcut(img, rect, n_iter=5):
 # NEEDS TO ADD n components=5
 def initalize_GMMs(img, mask):
     # TODO: implement initalize_GMMs
-    BGkmeans = KMeans(5, random_state=0)
-    FGkmeans = KMeans(5, random_state=0)
+    BGkmeans = KMeans(CLUSTERS_NUM, random_state=0)
+    FGkmeans = KMeans(CLUSTERS_NUM, random_state=0)
     bgGMM = BGkmeans.fit(img[mask==GC_BGD]).labels_
     fgGMM = FGkmeans.fit(img[mask==GC_FGD or mask==GC_PR_FGD]).labels_
 
     return bgGMM, fgGMM
 
 
-def pi_func(alpha, cluster, bg):
-    if bg == True:
-        nominator = 
-        denominator = img[mask == GC_BGD].size
+
+
+def d_func(img, mask, weights, mean, cov, isBG):
+    if isBG == GC_BGD:
+        diffrence = img[mask == GC_BGD] - mean
+
+    diffrence = img[mask == GC_FGD] - mean
+    value = -np.log10(weights) + 0.5*np.log10(np.linalg.det(cov)) + \
+    0.5*np.dot(diffrence.T, np.dot(np.linalg.inv(cov), diffrence))
+    return value
+    
 
 
 def assign_GMMs(img, mask, bgGMM, fgGMM):
+    # bgGMM & fgGMM are in kmeans labels
+    bg_weights, bg_mean, bg_cov = bgGMM
+    fg_weights, fg_mean, fg_cov = fgGMM
+    bg_d_values = np.array([])
+    fg_d_values = np.array([])
     
-    return None
+    for i in range(CLUSTERS_NUM):
+        bg_d_values.append(d_func(img, mask, bg_weights[i], bg_mean[i], bg_cov[i], GC_BGD))
+        fg_d_values.append(d_func(img, mask, fg_weights[i], fg_mean[i], fg_cov[i], GC_FGD))
+
+    bg_d_values.T
+    fg_d_values.T
+
+    new_bg_assignments = np.argmin(bg_d_values, axis=1)
+    new_fg_assignments = np.argmin(fg_d_values, axis=1)
+
+    return new_bg_assignments, new_fg_assignments
 
 
-def learn_GMMs():
-    return None
+def learn_GMMs(img, mask, bgGMM_assign, fgGMM_assign):
+    bg_weights = np.bincount(bgGMM_assign) / bgGMM_assign.size
+    fg_weights = np.bincount(fgGMM_assign) / fgGMM_assign.size
+    bg_mean = np.array([np.mean(img[bgGMM_assign==i] for i in range(CLUSTERS_NUM))])
+    fg_mean = np.array([np.mean(img[fgGMM_assign==i] for i in range(CLUSTERS_NUM))])
+    bg_cov = np.array([np.cov(img[bgGMM_assign==i] for i in range(CLUSTERS_NUM))])
+    fg_cov = np.array([np.cov(img[fgGMM_assign==i] for i in range(CLUSTERS_NUM))])
+
+    bg_param = (bg_weights, bg_mean, bg_cov)
+    fg_param = (fg_weights, fg_mean, fg_cov)
+    
+    return bg_param, fg_param
+
 
 
 # Define helper functions for the GrabCut algorithm
 def update_GMMs(img, mask, bgGMM, fgGMM):
     # TODO: implement GMM component assignment step
-    
+    if not isinstance(bgGMM, tuple):      
+
+        # On first iteration, same fuctionality as learn GMMs
+        bg_param, fg_param = learn_GMMs(img, mask, bgGMM, fgGMM)
+
+        new_bg_assign, new_fg_assign = assign_GMMs(img, mask, bg_param, fg_param)
+    else:
+        new_bg_assign, new_fg_assign = assign_GMMs(img, mask, bgGMM, fgGMM)
+
+    bgGMM, fgGMM = learn_GMMs(img, mask, new_bg_assign, new_fg_assign)
+
     return bgGMM, fgGMM
+
+
+def beta(img):
+    #in order to avoid duoble calculation we calculate based on 4 way connectivity
+    left_neighbor_dist = np.square(img[:, 1:] - img[:, :-1])
+    upleft_neighbor_dist = np.square(img[1:, 1:] - img[:-1, :-1])
+    up_neighbor_dist = np.square(img[1:, :] - img[:-1, :])
+    upright_neighbor_dist = np.square(img[1:, :-1] - img[:-1, 1:])
+
+
+    nomenator = np.sum(left_neighbor_dist) + np.sum(upleft_neighbor_dist) + \
+    np.sum(up_neighbor_dist) + np.sum(upright_neighbor_dist)
+
+
+    # We need the size of all sub images, basically they are the size of 4 regualer images minus 
+    # 3 cloumns because of left, upleft and upright neighbors and minus 3 rows because of upleft,
+    # upright and up neighbors. we add 2 more pixels because of double subtruction of the edge
+    # pixels of the first row.
+    denomenator = (4 * img.shape[0] * img.shape[1]) - (3 * img.shape[1]) - (3 * img.shape[0]) + 2
+    
+    
+    expectation = nomenator / denomenator
+    return 1 / (2 * expectation)
 
 
 def calculate_mincut(img, mask, bgGMM, fgGMM):
